40fce7e05dd6b9df70d466bda3a64aa659b24fa9
Hüseyin Tuğrul büyükışık <huseyin.tugrul.buyukisik@gmail.com>
2023-07-02 21:50:29 +0300

    devlpmnt

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main()
         std::vector<float> dataPointsY;
         for (int i = 0; i < N; i++)
         {
-            float x = i  / (float) N;
+            float x = i / (float)N;
             float y = std::sqrt(x);
             dataPointsX.push_back(x);
             dataPointsY.push_back(y);
@@ -22,9 +22,9 @@ int main()
         // 5 parameters: c0,c1,c2,c3,c4 of polynomial y = c0 + c1*x + c2*x^2 + c3*x^3 + c4*x^4
         // 100000 clones in parallel
         UFSACL::UltraFastSimulatedAnnealing<5, 100000> sim(
-            std::string("#define NUM_POINTS ") + std::to_string(N) + 
+            std::string("#define NUM_POINTS ") + std::to_string(N) +
             std::string(
-            R"(
+                R"(
                 // 256 threads looping "numLoopIter" times to compute NUM_POINTS data points error (energy)
                 const int numLoopIter = (NUM_POINTS / WorkGroupThreads) + 1;
                 for(int i=0;i<numLoopIter;i++)
@@ -67,9 +67,9 @@ int main()
         float stopTemperature = 0.0001f; // the closer to zero, the higher the accuracy, the slower to solution
         float coolingRate = 1.05f;
         int numReHeating = 5; // when single cooling is not enough, re-start the process multiple times while keeping the best solution
-        std::vector<float> prm = sim.run(startTemperature, stopTemperature, coolingRate, numReHeating,false,false,true);
-        
-        std::cout << "y = " << (prm[0]-0.5f)*1000.0f << " + (" << (prm[1] - 0.5f) * 1000.0f << " * x) + " << " (" << (prm[2] - 0.5f) * 1000.0f << " * x^2) + " << " (" << (prm[3] - 0.5f) * 1000.0f << " * x^3) + " << " (" << (prm[4] - 0.5f) * 1000.0f << " * x^4) "<< std::endl;
+        std::vector<float> prm = sim.run(startTemperature, stopTemperature, coolingRate, numReHeating, false, false, true);
+
+        std::cout << "y = " << (prm[0] - 0.5f) * 1000.0f << " + (" << (prm[1] - 0.5f) * 1000.0f << " * x) + " << " (" << (prm[2] - 0.5f) * 1000.0f << " * x^2) + " << " (" << (prm[3] - 0.5f) * 1000.0f << " * x^3) + " << " (" << (prm[4] - 0.5f) * 1000.0f << " * x^4) " << std::endl;
 
     }
     catch (std::exception& ex)
