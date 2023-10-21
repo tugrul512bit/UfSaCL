12cb004cb73d89cb6d3092fc09bd2da531ac0c4e
Hüseyin Tuğrul büyükışık <huseyin.tugrul.buyukisik@gmail.com>
2023-10-21 23:37:01 +0300

    a bigger neural network for sqrt approximation

--- main.cpp
+++ main.cpp
@@ -6,46 +6,37 @@ int main()
 {
     try
     {
+        // neural network architecture to learn y = sqrt(x) [1 input --> 1 output]
+        constexpr int nLayerInput = 1;
+        constexpr int nLayerHidden1 = 10;
+        constexpr int nLayerHidden2 = 20;
+        constexpr int nLayerHidden3 = 10;
+        constexpr int nLayerOutput = 1;
+        constexpr int nParams = (nLayerInput * 2) + (nLayerInput * nLayerHidden1 + nLayerHidden1) + (nLayerHidden1 * nLayerHidden2 + nLayerHidden2) + (nLayerHidden2 * nLayerHidden3 + nLayerHidden3) + (nLayerHidden3 * nLayerOutput + nLayerOutput);
+        std::vector<int> architecture = { nLayerInput,nLayerHidden1,nLayerHidden2,nLayerHidden3,nLayerOutput };
 
-        // train a neural-network to compute square-root of x
-        // single layer with 8 neurons + 1 output combining neuron with simple summation
-        // 16 (1 bias + 1 multiplier before tanh per neuron) + 8 (multipliers of output's input) + 1 (bias of output)
-        UFSACL::UltraFastSimulatedAnnealing<16 + 8 + 1, 5000> sim(R"(
+        
+
+        UFSACL::UltraFastSimulatedAnnealing<nParams, 500> sim(R"(
      
-                const int nData = numTrainingData[0];
-                double energyLocal = 0.0f;
+                const int nData = settings[0];
+                const int nLayers = settings[1];
+                float energyLocal = 0.0f;
 
                 // do same work for each pair of input-output & compute error (as energy for simulated annealing)
                 // this parallel for loop is not per workitem but works on all workitems at once, so any local variables (energyLocal) only visible by themselves
                 parallelFor(nData,
                 {
                         int i=loopId;
+                        float trainingDataInputTmp[1];
+                        float trainingDataOutputTmp[1];
 
-                        float output = 0.0f;
-                     
-                        for(int neuronId=0;neuronId<8;neuronId++)
-                        {
-                            
-                            // neuron bias
-                            const float bias = parameters[neuronId*2]*2 - 1;
-
-                            // neuron input multiplier
-                            const float mult = parameters[neuronId*2+1]*2 - 1;
-
-                            // neuron output multiplier (actually input of the last neuron on output)
-                            const float multOut = parameters[16+neuronId]*2 - 1;
-
-                            float result = tanh(mult * trainingInput[i] + bias) * multOut;  
-
-                            output += result;
-                        }
-
-                        // bias of output
-                        float result = tanh(output + parameters[16+8]*2 - 1);
-
-                        // how much error did it have against training data?                            
-                        float diff = result - trainingOutput[i]; 
-                        energyLocal += pow(fabs(diff),0.1f);
+                        trainingDataInputTmp[0] = trainingDataInput[i];
+                        trainingDataOutputTmp[0] = 0.0f;
+                        
+                        Compute(architecture, trainingDataInputTmp, trainingDataOutputTmp, nLayers, parameters);
+                        float diff = (trainingDataOutput[i] - trainingDataOutputTmp[i]);
+                        energy += diff*diff;
                         
                 });
 
@@ -53,22 +44,100 @@ int main()
                 
         )");
 
-        const int nTrainingData = 250000;
+        sim.addFunctionDefinition(R"(
+    
+            void Compute(global int * architecture, float * input, float * output, int numLayers, local float * parameters)
+            {
+                int parameterCtr = 0;
+                float layerVal[256];
+                float layerValTmp[256];
+                for(int i=0;i<numLayers;i++)
+                {
+                    if(i==0)
+                    {
+                        // input layer
+                        int n = architecture[i];
+                        for(int j=0;j<n;j++)
+                        {
+                            const float bias = parameters[parameterCtr++]*2.0f - 1.0f;
+
+                            // neuron input multiplier
+                            const float mult = parameters[parameterCtr++]*2.0f - 1.0f;
+
+                            // neuron output
+                            layerVal[j] = tanh(mult * input[j] + bias);  
+                        }                       
+
+                    }
+                    else if(i==numLayers-1)
+                    {
+                        // output layer
+                        int n = architecture[i];
+                        int n0 = architecture[i-1];
+                        for(int j=0;j<n;j++)
+                        {
+                            const float bias = parameters[parameterCtr++]*2.0f - 1.0f;
+                            float acc = 0.0f;
+                            for(int k=0;k<n0;k++)
+                            {
+                                // neuron input multiplier
+                                const float mult = parameters[parameterCtr++]*2.0f - 1.0f;
+
+                                // neuron output
+                                acc += mult * layerVal[k];  
+                            }
+
+                            output[j] = tanh(acc + bias);
+                        }  
+                    }
+                    else
+                    {
+                        // hidden layer
+                        int n = architecture[i];
+                        int n0 = architecture[i-1];
+                        for(int j=0;j<n;j++)
+                        {
+                            const float bias = parameters[parameterCtr++]*2.0f - 1.0f;
+                            float acc = 0.0f;
+                            for(int k=0;k<n0;k++)
+                            {
+                                // neuron input multiplier
+                                const float mult = parameters[parameterCtr++]*2.0f - 1.0f;
+
+                                // neuron output
+                                acc += mult * layerVal[k];  
+                            }
+
+                            layerValTmp[j] = tanh(acc + bias);
+                        }        
+
+                        for(int j=0;j<n;j++)               
+                            layerVal[j]=layerValTmp[j];
+
+                    }
+                }
+    
+            }
+
+        )");
+
+        const int nTrainingData = 250;
         std::vector<float> trainingDataInput(nTrainingData);
         std::vector<float> trainingDataOutput(nTrainingData);
-        std::vector<int> numTrainingData = { nTrainingData };
+        std::vector<int> settings = { nTrainingData,(int)architecture.size()};
         for (int i = 0; i < nTrainingData; i++)
         {
-            trainingDataInput[i] = i / (double)nTrainingData;
+            trainingDataInput[i] = (i)/(double) nTrainingData;
             trainingDataOutput[i] = std::sqrt((double)trainingDataInput[i]);
         }
-        sim.addUserInput("trainingInput", trainingDataInput);
-        sim.addUserInput("trainingOutput", trainingDataOutput);
-        sim.addUserInput("numTrainingData", numTrainingData);
+        sim.addUserInput("architecture", architecture);
+        sim.addUserInput("trainingDataInput", trainingDataInput);
+        sim.addUserInput("trainingDataOutput", trainingDataOutput);
+        sim.addUserInput("settings", settings);
         sim.build();
-        float startTemperature = 1.0f;
-        float stopTemperature = 0.001f;
-        float coolingRate = 1.5f;
+        float startTemperature = 10.0f;
+        float stopTemperature = 0.00001f;
+        float coolingRate = 1.005f;
         bool debugPerformance = false;
         bool debugDevice = false;
         bool debugEnergy = true;
@@ -76,45 +145,172 @@ int main()
         std::vector<float> prm = sim.run(
             startTemperature, stopTemperature, coolingRate, numReHeating,
             debugPerformance, debugDevice, debugEnergy,
-            [](float* optimizedParameters) {
-                // callback that is called whenever a better(lower) energy is found
-                // do something with the optimized parameters
-                float inp = 0.5f;
-                float out = 0.0f;
-                for (int i = 0; i < 8; i++)
+            [&](float* optimizedParameters) {
+
+                float input[1] = { 0.5f };
+                float output[1] = { 0.0f };
+                float* parameters = optimizedParameters;
                 {
-                    const float bias = optimizedParameters[i * 2] * 2 - 1;
+                    int parameterCtr = 0;
+                    float layerVal[256];
+                    float layerValTmp[256];
+                    for (int i = 0; i < architecture.size(); i++)
+                    {
+                        if (i == 0)
+                        {
+                            // input layer
+                            int n = architecture[i];
+                            for (int j = 0; j < n; j++)
+                            {
+                                const float bias = parameters[parameterCtr++] * 2.0f - 1.0f;
 
-                    // neuron input multiplier
-                    const float mult = optimizedParameters[i * 2 + 1] * 2 - 1;
+                                // neuron input multiplier
+                                const float mult = parameters[parameterCtr++] * 2.0f - 1.0f;
 
-                    // neuron output multiplier (actually input of the last neuron on output)
-                    const float multOut = optimizedParameters[16 + i] * 2 - 1;
+                                // neuron output
+                                layerVal[j] = tanh(mult * input[j] + bias);
+                            }
 
-                    out += std::tanh(mult * inp + bias) * multOut;
+                        }
+                        else if (i == architecture.size() - 1)
+                        {
+                            // output layer
+                            int n = architecture[i];
+                            int n0 = architecture[i - 1];
+                            for (int j = 0; j < n; j++)
+                            {
+                                const float bias = parameters[parameterCtr++] * 2.0f - 1.0f;
+                                float acc = 0.0f;
+                                for (int k = 0; k < n0; k++)
+                                {
+                                    // neuron input multiplier
+                                    const float mult = parameters[parameterCtr++] * 2.0f - 1.0f;
+
+                                    // neuron output
+                                    acc += mult * layerVal[k];
+                                }
+
+                                output[j] = tanh(acc + bias);
+                            }
+                        }
+                        else
+                        {
+                            // hidden layer
+                            int n = architecture[i];
+                            int n0 = architecture[i - 1];
+                            for (int j = 0; j < n; j++)
+                            {
+                                const float bias = parameters[parameterCtr++] * 2.0f - 1.0f;
+                                float acc = 0.0f;
+                                for (int k = 0; k < n0; k++)
+                                {
+                                    // neuron input multiplier
+                                    const float mult = parameters[parameterCtr++] * 2.0f - 1.0f;
+
+                                    // neuron output
+                                    acc += mult * layerVal[k];
+                                }
+
+                                layerValTmp[j] = tanh(acc + bias);
+                            }
+
+                            for (int j = 0; j < n; j++)
+                                layerVal[j] = layerValTmp[j];
+
+                        }
+                    }
+
+
+                    std::cout << "sqrt (0.5) =" << output[0] << std::endl;
+
                 }
-                std::cout << "sqrt(0.5f)=" << std::tanh(out + (optimizedParameters[16 + 8] * 2 - 1)) << std::endl;
                 std::cout << "------" << std::endl;
             }
         );
 
-        for (float inp = 0.1515; inp < 0.9595; inp += 0.1)
+        for (float inp = 0.15; inp < 0.95; inp += 0.1)
         {
 
-            float out = 0.0f;
-            for (int i = 0; i < 8; i++)
+
+            float input[1] = { inp };
+            float output[1] = { 0.0f };
+            float* parameters = prm.data();
             {
-                const float bias = prm[i * 2] * 2 - 1;
+                int parameterCtr = 0;
+                float layerVal[256];
+                float layerValTmp[256];
+                for (int i = 0; i < architecture.size(); i++)
+                {
+                    if (i == 0)
+                    {
+                        // input layer
+                        int n = architecture[i];
+                        for (int j = 0; j < n; j++)
+                        {
+                            const float bias = parameters[parameterCtr++] * 2.0f - 1.0f;
 
-                // neuron input multiplier
-                const float mult = prm[i * 2 + 1] * 2 - 1;
+                            // neuron input multiplier
+                            const float mult = parameters[parameterCtr++] * 2.0f - 1.0f;
 
-                // neuron output multiplier (actually input of the last neuron on output)
-                const float multOut = prm[16 + i] * 2 - 1;
+                            // neuron output
+                            layerVal[j] = tanh(mult * input[j] + bias);
+                        }
 
-                out += std::tanh(mult * inp + bias) * multOut;
+                    }
+                    else if (i == architecture.size() - 1)
+                    {
+                        // output layer
+                        int n = architecture[i];
+                        int n0 = architecture[i - 1];
+                        for (int j = 0; j < n; j++)
+                        {
+                            const float bias = parameters[parameterCtr++] * 2.0f - 1.0f;
+                            float acc = 0.0f;
+                            for (int k = 0; k < n0; k++)
+                            {
+                                // neuron input multiplier
+                                const float mult = parameters[parameterCtr++] * 2.0f - 1.0f;
+
+                                // neuron output
+                                acc += mult * layerVal[k];
+                            }
+
+                            output[j] = tanh(acc + bias);
+                        }
+                    }
+                    else
+                    {
+                        // hidden layer
+                        int n = architecture[i];
+                        int n0 = architecture[i - 1];
+                        for (int j = 0; j < n; j++)
+                        {
+                            const float bias = parameters[parameterCtr++] * 2.0f - 1.0f;
+                            float acc = 0.0f;
+                            for (int k = 0; k < n0; k++)
+                            {
+                                // neuron input multiplier
+                                const float mult = parameters[parameterCtr++] * 2.0f - 1.0f;
+
+                                // neuron output
+                                acc += mult * layerVal[k];
+                            }
+
+                            layerValTmp[j] = tanh(acc + bias);
+                        }
+
+                        for (int j = 0; j < n; j++)
+                            layerVal[j] = layerValTmp[j];
+
+                    }
+                }
+
+
+               
+
             }
-            std::cout << "sqrt(" << inp << ")=" << std::tanh(out + (prm[16 + 8] * 2 - 1)) << "  error = " << (std::tanh(out + (prm[16 + 8] * 2 - 1)) - std::sqrt(inp)) / std::sqrt(inp) * 100 << "%" << std::endl;
+            std::cout << "------" << std::endl;
+            std::cout << "sqrt(" << input[0] << ")=" << output[0] << "  error = " << (output[0] - std::sqrt(input[0])) / std::sqrt(input[0]) * 100 << "%" << std::endl;
         }
 
     }
